b208a4929bc3b61e6b9ba0a50e91430e6d5eb3a0
NasrinAkter22 <258nasrinakter@gmail.com>
2024-06-24 17:03:50 +0600

    this my nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,43 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
+
 
   return (
     <>
-     
+      
       <h1>Vite + React</h1>
       <Person></Person>
       <Person></Person>
       <Person></Person>
       <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
- 
-     
+      <Student></Student>
+      
+    
+      
+      
     </>
   )
 }
 function Person(){
-  const age=30;
-  const family=2;
-  const job=2;
-  const home="yes";
+  const age = 27;
+  const money=30;
+const person={name:"Sakib",age:30}
+
+
+return <h4>i am {person.name} with age:{age+money}</h4>
+
+
+
   
-  
-  return(
-    <>
-    <h1>ismam:{name} with age:{age}</h1>
-    <h1>family:{family}</h1>
-    <h1>job:{job}</h1>
-    <h1>home:{home}</h1>
-    </>
-  )
 }
-function Developer(){
+function Student(){
+   
   return(
     <div>
-      <h1>dev</h1>
+<h1>this is a student</h1>
+<p>name:</p>
+<p>age:</p>
     </div>
   )
 }
